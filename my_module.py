d64bcbafe89058aa04eefdef2c4f9dbf841ed989
Lucas <mrpiratex35@gmail.com>
2026-05-16 04:22:06 -0300

    testando modulo random

---
+++ my_module.py
@@ -0,0 +1,13 @@
+"""
+May 16th, 2026
+
+Tails head Inc.
+"""
+import random
+
+random_heads_or_tails = random.randint(0, 1)
+
+if random_heads_or_tails == 0:
+    print(f"{random_heads_or_tails} Heads")
+else:
+    print(f"{random_heads_or_tails} Tails")
---
+++ randomizer.py
@@ -0,0 +1,12 @@
+import random
+
+random_integer = random.randint(1, 2026)
+
+if random_integer >= 1000:
+    print(f"Your year is {random_integer} A.D")
+
+elif random_integer == 476:
+    print(f"{random_integer} A.D YOOOOOO ROMAN JUST FELL")
+else:
+    print(f"Your year is {random_integer} B.C")
+
